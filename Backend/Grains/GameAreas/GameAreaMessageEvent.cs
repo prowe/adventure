971c57414e3071ca98e5d2b093964eb47dc4a92b
Paul Rowe <prowe@sourceallies.com>
2018-06-10 13:44:31 -0500

    using logger

--- Backend/Grains/GameAreas/GameAreaMessageEvent.cs
+++ Backend/Grains/GameAreas/GameAreaMessageEvent.cs
@@ -3,5 +3,10 @@ namespace GameAreas
     public class GameAreaMessageEvent
     {
         public string Message {get; set;}
+
+        public override string ToString()
+        {
+            return $"GameAreaMessageEvent: {Message}";
+        }
     }
 }
--- Backend/Silo/GameAreas/GameAreaGrain.cs
+++ Backend/Silo/GameAreas/GameAreaGrain.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using Orleans;
 using Orleans.Streams;
 
@@ -7,23 +8,28 @@ namespace GameAreas
 {
     public class GameAreaGrain : Grain, IGameAreaGrain
     {
+        private readonly ILogger<GameAreaGrain> logger;
         private IAsyncStream<GameAreaMessageEvent> areaStreamProvider;
 
+        public GameAreaGrain(ILogger<GameAreaGrain> logger)
+        {
+            this.logger = logger;
+        }
         public override async Task OnActivateAsync()
         {
             System.Console.WriteLine("Activating");
             await base.OnActivateAsync();
             base.RegisterTimer(TickTock, null, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(2));
-            areaStreamProvider = GetStreamProvider("SMSProvider").GetStream<GameAreaMessageEvent>(Guid.Empty, null);
+            areaStreamProvider = GetStreamProvider("SMSProvider").GetStream<GameAreaMessageEvent>(GrainReference.GetPrimaryKey(), null);
         }
 
         private async Task TickTock(object arg)
         {
-            var message = $"The clock ticks. The time is now _{DateTime.Now}_";
-            System.Console.WriteLine(message);
-            await areaStreamProvider.OnNextAsync(new GameAreaMessageEvent {
-                Message = message
-            });
+            var e = new GameAreaMessageEvent {
+                Message = $"The clock ticks. The time is now _{DateTime.Now}_"
+            };
+            logger.LogInformation("Sending event: {e}", e);
+            await areaStreamProvider.OnNextAsync(e);
         }
 
         public Task Initialize()
